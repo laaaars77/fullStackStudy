c30dd9501023d93364edcdf4bf483f60d0665a81
Jens Ludwig <jens.lu@web.de>
2025-11-19 16:48:12 +0100

    trying a print statement

--- backend/app/websocket_handlers.py
+++ backend/app/websocket_handlers.py
@@ -3,6 +3,7 @@ from aiohttp import web
 async def websocket_handler(request):
     ws = web.WebSocketResponse()
     await ws.prepare(request)
+    print("I see you!")
     async for msg in ws:
         if msg.type == web.WSMsgType.TEXT:
             await ws.send_str(f"Echo: {msg.data}")
