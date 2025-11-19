3adcc89cc85ab5c6d2a7a4dc7e5c67facfbcddaa
Jens Ludwig <jens.lu@web.de>
2025-11-19 16:36:14 +0100

    main.py deleted and clearance of unnecessary prints

--- backend/app/websocket_handlers.py
+++ backend/app/websocket_handlers.py
@@ -3,14 +3,7 @@ from aiohttp import web
 async def websocket_handler(request):
     ws = web.WebSocketResponse()
     await ws.prepare(request)
-
-    print("Client connected!")  # <-- hinzufügen
-
     async for msg in ws:
-        print("Received:", msg)  # <-- hinzufügen
-
         if msg.type == web.WSMsgType.TEXT:
             await ws.send_str(f"Echo: {msg.data}")
-
     return ws
-
